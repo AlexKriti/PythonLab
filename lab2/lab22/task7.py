599adc6ae0b0eb367a0fe631dc0198ae064be017
AlexKriti <kap293654406@mail.ru>
2025-11-18 21:52:52 +0300

    Edited several tasks

--- lab2/lab22/task7.py
+++ lab2/lab22/task7.py
@@ -1,36 +1,73 @@
-# определяем функцию
+# # определяем функцию
 
-def merged_sorted_list(lst1, lst2): 
+# def merged_sorted_list(lst1, lst2): 
 
-    # инициализируем начальные данные
+#     # инициализируем начальные данные
 
+#     lst = []
+#     i = 0 
+#     j = 0 
+
+#     # проходим основной цикл слияния(сравниваем/добавляем элементы)
+
+#     while i < len(lst1) and j < len(lst2) : 
+#         if lst1[i] <= lst2[j]: 
+#             lst.append(lst1[i])
+#             i+= 1 
+#         else: 
+#             lst.append(lst2[j])
+#             j+=1 
+
+#     # добавляем оставшиеся элементы
+
+#     while i < len(lst1): 
+#         lst.append(lst1[i])
+#         i+=1
+#     while j < len(lst2): 
+#         lst.append(lst2[j]) 
+#         j+=1
+#     return lst 
+
+# # пример для исполнения
+
+# lst1= [1, 2, 3]
+# lst2 = [-1, 0, 1, 2, 6]
+# res_lst = merged_sorted_list(lst1, lst2)
+# print(res_lst)
+
+def merged_sorted_list(lst1, lst2):
+    """Функция для слияния двух отсортированных списков в один отсортированный список"""
     lst = []
-    i = 0 
-    j = 0 
+    i = 0
+    j = 0
 
-    # проходим основной цикл слияния(сравниваем/добавляем элементы)
-
-    while i < len(lst1) and j < len(lst2) : 
-        if lst1[i] <= lst2[j]: 
+    while i < len(lst1) and j < len(lst2):
+        if lst1[i] <= lst2[j]:
             lst.append(lst1[i])
-            i+= 1 
-        else: 
+            i += 1
+        else:
             lst.append(lst2[j])
-            j+=1 
+            j += 1
 
-    # добавляем оставшиеся элементы
-
-    while i < len(lst1): 
+    while i < len(lst1):
         lst.append(lst1[i])
-        i+=1
-    while j < len(lst2): 
-        lst.append(lst2[j]) 
-        j+=1
-    return lst 
+        i += 1
+    while j < len(lst2):
+        lst.append(lst2[j])
+        j += 1
+    return lst
 
-# пример для исполнения
+# Ввод данных от пользователя
+print("Введите элементы первого отсортированного списка через пробел:")
+lst1 = list(map(int, input().split()))
 
-lst1= [1, 2, 3]
-lst2 = [-1, 0, 1, 2, 6]
-res_lst = merged_sorted_list(lst1, lst2)
-print(res_lst)
+print("Введите элементы второго отсортированного списка через пробел:")
+lst2 = list(map(int, input().split()))
+
+# Сортировка списков на случай, если пользователь ввел неотсортированные данные
+lst1.sort()
+lst2.sort()
+
+# Слияние и вывод результата
+result = merged_sorted_list(lst1, lst2)
+print(f"Результат слияния: {result}")
